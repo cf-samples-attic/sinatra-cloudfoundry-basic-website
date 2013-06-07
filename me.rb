cac067b1a774fff27501a1763cddfb1d005902f8
Scott Frederick <scottyfred@gmail.com>
2013-06-07 11:46:02 -0500

    Changes to run on Cloud Foundry v2 with ruby buildpack.

--- me.rb
+++ me.rb
@@ -5,15 +5,18 @@ require "hashie"
 require "yaml"
 require "sinatra/url_for"
 
-get "/" do
-  @me = YAML.load_file("poco.yml")
-  @me = Hashie::Mash.new(@me)
-  @full_url = url_for("/", :full)
-  @description = "Who is #{@me.preferred_username || @me.display_name} ?"
-  @image = url_for(@me.photos.first.value, :full)
-  @title = @me.display_name
-  @appid = ENV['facebook_app_id']
-  haml :index
-end
+class AboutMe < Sinatra::Base
+  helpers Sinatra::UrlForHelper
 
+  get "/" do
+    @me = YAML.load_file("poco.yml")
+    @me = Hashie::Mash.new(@me)
+    @full_url = url_for("/", :full)
+    @description = "Who is #{@me.preferred_username || @me.display_name} ?"
+    @image = url_for(@me.photos.first.value, :full)
+    @title = @me.display_name
+    @appid = ENV['facebook_app_id']
+    haml :index
+  end
+end
 
